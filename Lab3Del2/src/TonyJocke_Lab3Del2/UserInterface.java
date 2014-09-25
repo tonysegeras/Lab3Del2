35b2d16f370227f62f6ccb02dd43d5dce1233316
joalon <joakimlonnegren@gmail.com>
2014-09-25 15:00:34 +0200

    Adds UserInterface.java with content

---
+++ Lab3Del2/src/TonyJocke_Lab3Del2/UserInterface.java
@@ -0,0 +1,23 @@
+package TonyJocke_Lab3Del2;
+
+import java.util.Scanner;
+
+public class UserInterface {
+	private String printedMenu;
+	private Scanner scan;
+	
+	public UserInterface(){
+		this.printedMenu = "Welcome, please choosen an option:\n" +
+				"1: Add a book to the library.\n" +
+				"2: Remove a book from the library" +
+				"3: Search the library for a title.\n" +
+				"4: Search the library.\n" +
+				"5: List all the books in the library.\n" +
+				"6: Save and exit.\n";
+		this.scan = new Scanner(System.in);
+	}
+	
+	public void printMenu(){
+		System.out.println(printedMenu);
+	}	
+}
