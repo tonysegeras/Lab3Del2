3cf27ac607460d79153c3ca168fcc9db881c8d8e
joalon <joakimlonnegren@gmail.com>
2014-09-26 16:11:17 +0200

    Starts to implement serializing

--- Lab3Del2/src/TonyJocke_Lab3Del2/LibraryMain.java
+++ Lab3Del2/src/TonyJocke_Lab3Del2/LibraryMain.java
@@ -1,23 +1,21 @@
 package TonyJocke_Lab3Del2;
 
-import java.util.ArrayList;
-
 public class LibraryMain {
 	public static void main(String[] args) {
 		
-		//Tests
-		ArrayList<Author> newAuthor = new ArrayList<Author>();
-		newAuthor.add(new Author("William Golding"));
-		Book myNewBook = new Book("1234", "Lord of the flies", 1, 14.99, newAuthor);
-		System.out.println(myNewBook.toString());
-		
-		// Create stuff & load books
+		// Initialize
 		CollectionOfBooks library = new CollectionOfBooks();
 		
+		// Load books
+		String filename = args[0];
+		
+		
+		// Begin main program execution
 		UserInterface UI = new UserInterface();
 		UI.menu(library);
 		
-		// Save books
-		System.out.println(library.toString());
+		// Save books when user wants to quit
+		
+		
 	}
 }
