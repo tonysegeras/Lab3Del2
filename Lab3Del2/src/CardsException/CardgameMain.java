7a17e059f56f6aac44ec1e87c83d560d21e1a346
joalon <joakimlonnegren@gmail.com>
2014-09-29 12:08:04 +0200

    1

---
+++ Lab3Del2/src/CardsException/CardgameMain.java
@@ -0,0 +1,9 @@
+package CardsException;
+
+public class CardgameMain {
+	public static void main(String[] args) {
+		
+		
+
+	}
+}
---
+++ Lab3Del2/src/CardsException/NoSuchCardException.java
@@ -0,0 +1,6 @@
+package CardsException;
+
+public class NoSuchCardException extends java.lang.RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+}
