fd4ae5114411ca7b6ef3b6f121baddc0446c06fb
Tony Segerås <tonysegeras@gmail.com>
2014-09-30 18:14:32 +0200

    Fixed with UML-diagram, started to comment code

--- Lab3Del2/src/TonyJocke_Lab3Del2/Author.java
+++ Lab3Del2/src/TonyJocke_Lab3Del2/Author.java
@@ -3,26 +3,29 @@ package TonyJocke_Lab3Del2;
 import java.io.Serializable;
 
 public class Author implements Serializable {
-	
+
 	private String name;
-	
+
+	/**
+	 * Constructor making an author with the specified name
+	 */
 	public Author(String name) {
 		this.name = new String(name);
 	}
-	
-	public void setAuthor(String name){
+
+	public void setAuthor(String name) {
 		this.name = name;
 	}
-	
+
 	public String getAuthor() {
 		String info = this.name;
 		return info;
 	}
-	
+
 	public String toString() {
 		String info = this.name;
 		return info;
 	}
-	
+
 	private static final long serialVersionUID = 1L;
 }
