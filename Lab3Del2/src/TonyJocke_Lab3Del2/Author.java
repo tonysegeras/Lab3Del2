757a73632cbc768a3a83bf1090952db7c9c9b9e5
Tony Segerås <tonysegeras@gmail.com>
2014-09-30 18:57:23 +0200

    Continued to comment code

--- Lab3Del2/src/TonyJocke_Lab3Del2/Author.java
+++ Lab3Del2/src/TonyJocke_Lab3Del2/Author.java
@@ -7,21 +7,34 @@ public class Author implements Serializable {
 	private String name;
 
 	/**
-	 * Constructor making an author with the specified name
+	 * Constructor making an author with the specified name.
+	 * @param name the name the author gets
 	 */
 	public Author(String name) {
 		this.name = new String(name);
 	}
-
+	
+	/**
+	 * Void-method to set an authors name.
+	 * @param name the name the author gets
+	 */
 	public void setAuthor(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Method for getting the authors name. 
+	 * @return a string representation of the name
+	 */
 	public String getAuthor() {
 		String info = this.name;
 		return info;
 	}
 
+	/**
+	 * Method to print out the name of the author to a string.
+	 * @return a string representation of the name
+	 */
 	public String toString() {
 		String info = this.name;
 		return info;
--- Lab3Del2/src/TonyJocke_Lab3Del2/Book.java
+++ Lab3Del2/src/TonyJocke_Lab3Del2/Book.java
@@ -10,6 +10,14 @@ public class Book implements Comparable<Book>, Serializable {
 	private double price;
 	private ArrayList<Author> authors;
 
+	/**
+	 * Constructor for setting up a book by giving parameters.
+	 * @param isbn to set the ISBN of the book
+	 * @param title to set the title of the book
+	 * @param edition to set the edition of the book
+	 * @param price to set the price of the book
+	 * @param authorAdd gives an Arraylist of authors to set to the book 
+	 */
 	public Book(String isbn, String title, int edition, double price,
 			ArrayList<Author> authorAdd) {
 		this.isbn = new String(isbn);
@@ -22,6 +30,10 @@ public class Book implements Comparable<Book>, Serializable {
 		}
 	}
 	
+	/**
+	 * Constructor for setting up a book from a book object.
+	 * @param book the book that the book take it's information from to set up the book
+	 */
 	public Book(Book book) {
 		this.isbn = new String(book.getIsbn());
 		this.title = new String(book.getTitle());
@@ -33,21 +45,36 @@ public class Book implements Comparable<Book>, Serializable {
 			this.authors.add(new Author(a.getAuthor()));
 	}
 	
+	/**
+	 * Sets the title of the book and the name of the authors of the book to lower case letters.
+	 */
 	public void toLowerCase(){
 		this.title.toLowerCase();
 		for(Author a: authors)
 			a.setAuthor( a.getAuthor().toLowerCase() );
 	}
 
+	/**
+	 * Sends a string to a method that creates an author object to the list of authors.
+	 * @param author the string parameter that sets the name of the author to the book
+	 */
 	public void addAuthor(String author) {
 		authors.add(new Author(author));
 	}
 
+	/**
+	 * Gets a list of the authors of the book.
+	 * @return returns an ArrayList reference of the authors of the book
+	 */
 	public ArrayList<Author> getAuthors() {
 		ArrayList<Author> copy = authors;
 		return copy;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getIsbn() {
 		String info = this.isbn;
 		return info;
